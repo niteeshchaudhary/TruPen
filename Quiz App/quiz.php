c2747a00b27b357bd0c1e93ada42cbeafd5ab213
Tabish Khalid Halim <tkhalim14@gmail.com>
2021-10-04 20:57:22 +0530

    QUIZ UI
    Major Update Issue : Koi alignment set kardo and push code

--- Quiz App/quiz.php
+++ Quiz App/quiz.php
@@ -15,6 +15,8 @@ $qryst="select * from ".$_GET["quiz_choosed"].";";
 ?>
 <html>
 <head>
+<title><?php echo "Quiz : ".$_GET["quiz_choosed"]; ?></title>
+<link rel="stylesheet" href="style.css">
 <style>
   * {box-sizing: border-box}
 body {font-family: Verdana, sans-serif; margin:0}
@@ -202,15 +204,199 @@ button{
   font-family: inherit;
   padding: 1.3rem;
 }
+.container {
+    position: relative;
+    padding: 50px;
+    width: 260px;
+    min-height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(5px);
+    border-radius: 10px;
+    box-shadow: 0 25px 45px rgba(0, 0, 0, 0.2);
+}
+
+
+/* Add a black background color to the top navigation */
+
+.topnav {
+    border-radius: 25px;
+    position: sticky;
+    background-color: lightgray;
+    overflow: hidden;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+}
+
+
+/* Style the links inside the navigation bar */
+
+.topnav a {
+    float: left;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+
+/* Change the color of links on hover */
+
+.topnav a:hover {
+    border-radius: 25px;
+    background-color: rgba(255, 251, 251, 0.418);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+
+/* Add a color to the active/current link */
+
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;600;700&display=swap");
+ :root {
+    --color-v: #7f71fe;
+    --color-m: #7e00ff;
+    --color-e: #ffbe3c;
+}
+
+.topnav a.active {
+    font-weight: 400;
+    background-color: #4285F4;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+}
+
+@keyframes AnimationName {
+    0% {
+        background-position: 0% 50%
+    }
+    50% {
+        background-position: 100% 50%
+    }
+    100% {
+        background-position: 0% 50%
+    }
+}
+
+.topnav a.active:hover {
+    border-radius: 0px;
+    font-weight: 400;
+    background: linear-gradient(270deg, #4285f4, #7542f4, #eb42f4);
+    background-size: 600% 600%;
+    -webkit-animation: AnimationName 3s ease infinite;
+    -moz-animation: AnimationName 3s ease infinite;
+    animation: AnimationName 3s ease infinite;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+}
+
+.gradient {
+    -webkit-text-stroke: 0.1pt white;
+    font-family: "Comfortaa", cursive;
+    background-image: -webkit-gradient(linear, left top, right top, from(var(--color-m)), color-stop(var(--color-e)), color-stop(var(--color-m)), to(var(--color-v)));
+    background-image: -webkit-linear-gradient(left, var(--color-m) 0%, var(--color-e) 50%, var(--color-m) 150%, var(--color-v) 200%);
+    background-image: linear-gradient(to right, var(--color-m) 0%, var(--color-e) 50%, var(--color-m) 150%, var(--color-v) 200%);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: rgb(210, 210, 210);
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #000000;
+    display: block;
+    transition: 0.3s;
+}
+
+.sidenav a:hover {
+    color: #aefff4;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {
+        padding-top: 15px;
+    }
+    .sidenav a {
+        font-size: 18px;
+    }
+}
+
+@keyframes rotate {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(-360deg);
+    }
+}
+
+.rotation {
+    animation-name: rotate;
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+}
 </style>
 </head>
 <body>
+        <center>
+            <div class="container">
+                <h1><span style="font-size:30px;cursor:pointer" onclick="toggle();"> <div style = "display:inline-block;" id="rotation">&#9776; </div> Dashboard</span></h1>
+            </div>
+            <br><br>
+            <div class="topnav">
+                <a class="active gradient-text" href="loggedin.php"><img src="../Image_Components/truPen Better Logo.png" style="width: 25pt;">
+                    <div style="display:inline-block;" class="gradient">truPen</div>
+                </a>
+                &nbsp;
+                <a href="Quiz App/select.php">Quizzes</a>
+                <a href="Quiz App/create.php">Create Quiz</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        </center>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
   <div class="slideshow-container">
     <?php
 
     if ($result && $result->num_rows > 0) {
     for ($x = 0;$row = $result->fetch_assoc(); $x++) {
-    echo '<div class="mySlides fade">
+    echo '<div class="mySlides fade" id="'.$x.'">
             <div class="quiz-container" id="quiz">
               <div class="quiz-header">
                 <div class="numbertext">'.($x+1).' /'.$result->num_rows.'</div>
@@ -226,24 +412,103 @@ button{
           </div>';
     }}
     ?>
-    <a  class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    <a class="first" onclick="firstSlide()">&#10094;&#10094;</a>
-    <a class="last" onclick="lastSlide()">&#10095;&#10095;</a>
-      <div class="text">Submit</div>
-      <div style="text-align:center">
-        <?php
-
-        ?> 
+    <a  class="prev" onclick="plusDivs(-1)">&#10094;</a>
+    <a class="next" onclick="plusDivs(1)">&#10095;</a>
+    <a class="first" onclick="first()">&#10094;&#10094;</a>
+    <a class="last" onclick="last()">&#10095;&#10095;</a>
+      <div style="text-align:left">
+      <div class="text"><a href="#" style="text-decoration: none;color:white;" id="blink">Submit</div>
       </div>
   </div>
   <br>
-<a href="newupload.php"><button>Upload Image</button></a>
+  <br>
 <br>
-<form action="logout.php">
 <br>
-<input type="submit" value="Log Out">
+<br>
+<a href="newupload.php" style="text-decoration: none;" id = "blink2"><button>Upload Image</button></a>
+<br>
+<br>
+<br>
+<br>
+<form action="http://localhost/assperl/loggedin.php">
+<br>
+<button type="submit" id = "submit">Exit Quiz</button>
 </form>
-<script src="sldshw.js"></script>
+<script>
+var slideIndex = 1;
+var val;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    val = n;
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        x[i].classList.add("left");
+    }
+    if (val != 1) {
+        x[slideIndex - 1].classList.remove("left");
+        x[slideIndex - 1].classList.add("right");
+    } else {
+        x[slideIndex - 1].classList.remove("right");
+        x[slideIndex - 1].classList.add("left");
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+function first(){
+    var x = document.getElementsByClassName("mySlides");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    if(x[0].classList.item(0) != "left"){
+        x[0].classList.remove("right");
+        x[0].classList.add("left");
+    }
+    x[0].style.display = "block";
+}
+
+function last() {
+    var x = document.getElementsByClassName("mySlides");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    if(x[x.length-1].classList.item(0) != "right"){
+        x[x.length-1].classList.remove("left");
+        x[x.length-1].classList.add("right");
+    }
+    x[x.length-1].style.display = "block";
+}
+function toggle() {
+                var x = document.getElementById("mySidenav");
+                var y = document.getElementById("rotation");
+                if (x.style.width === "250px") {
+                    y.classList.add("rotation");
+                    closeNav();
+                } else {
+                    y.classList.add("rotation");
+                    openNav();
+                }
+                setTimeout(() => {
+                    y.classList.remove("rotation");
+                }, 1050);
+            }
+
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "250px";
+            }
+
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+            }
+</script>
 </body>
 </html>
