8f2b4611459836ec8934c3a1e3a2f5feb73fb318
Tabish Khalid Halim <tkhalim14@gmail.com>
2021-10-20 12:31:07 +0530

    Minor update for chat bot

--- Chatbot/static/scripts/responses.js
+++ Chatbot/static/scripts/responses.js
@@ -1,14 +1,8 @@
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
+    //Add the responses here :)
+    input = input.toLowerCase();
 
-    // Simple responses
+
     if (input == "hello") {
         return "Hello there!";
     } else if (input == "goodbye") {
