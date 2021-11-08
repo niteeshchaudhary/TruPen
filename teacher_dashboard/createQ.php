2194148aa55746a1f0b113eeb7d956f0bd794214
niteeshchaudhary <nkchaudhary00@gmail.com>
2021-11-08 14:30:25 +0530

    message icon

--- teacher_dashboard/createQ.php
+++ teacher_dashboard/createQ.php
@@ -136,10 +136,10 @@
 					</a>
 					<a href="notif.php" class="app-sidebar-link">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-							<rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
-							<line x1="16" y1="2" x2="16" y2="6" />
-							<line x1="8" y1="2" x2="8" y2="6" />
-							<line x1="3" y1="10" x2="21" y2="10" />
+						<rect x="2" y="4" width="20" height="18" rx="2" ry="2" />
+							<line x1="3" y1="4" x2="12" y2="16" />
+							<line x1="12" y1="16" x2="21" y2="4" />
+							<line x1="3" y1="16" x2="21" y2="16" />
 						</svg>
 					</a>
 					<a href="print_req.php" class="app-sidebar-link">
