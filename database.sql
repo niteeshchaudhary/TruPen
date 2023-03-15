692f7d5397fbc708815035a58664703d3910442d
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2023-03-15 09:36:52 +0530

    added use

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS trupendb;
-
+use trupendb;
 CREATE TABLE IF NOT EXISTS user
             (username varchar(120) PRIMARY KEY,
             passcode varchar(120),
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS user
             );
 
 INSERT INTO user(username, passcode, firstname, lastname, email, gender, birthday, bio,img_dir, cost) VALUES ('user', 'pass', 'foo', 'bar', 'user@gmail.com', 'male','2000-08-14', 'fantastic person', '../profile_pic/student/user.jpg', '0');
-        $con->query($sql);
+
 
 INSERT INTO user(username, passcode, firstname, lastname, email, gender, birthday,bio, img_dir, cost) VALUES ('eval', 'eva', 'eval', 'eva', 'eval@gmail.com', 'female','2003-05-17', 'cool person', '../profile_pic/student/eval.jpg', '0');
        
